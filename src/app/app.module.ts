1034d3fe7325e9750d6c7b40072d2be7a13952b3
Andres Reynoso <u202010196@upc.edu.pe>
2022-05-05 17:09:45 -0500

    Added other Components
    Added more Components to do work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,25 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppointmentsComponent} from "./appointments/pages/appointments/appointments.component";
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import { ClientsComponent } from './clients/pages/clients/clients.component';
+import { PagesComponent } from './notifications/pages/notifications/pages.component';
+import { IndexComponent} from "./index/pages/index.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyLawyerNavigationComponent
+    MyLawyerNavigationComponent,
+    AppointmentsComponent,
+    ClientsComponent,
+    PagesComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +38,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clients/pages/clients/clients.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+  panelOpenState = false;
+  list = [1, 2, 3, 4, 5, 6];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
