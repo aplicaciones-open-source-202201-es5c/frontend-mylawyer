aceaf6ea0ff28f1ebe8b31042219f2e60c566ec5
Diego <diegotasa35@hotmail.com>
2022-05-05 23:06:30 -0500

    configurations on services and models

--- src/app/notifications/pages/notifications/pages.component.ts
+++ src/app/notifications/pages/notifications/pages.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {notificationsService} from "../../model/notifications.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  notificationsData: Notification;
+  dataSource:MatTableDataSource<any>;
+  displayedColumns:String[]=['id','name','date','actions'];
+  isEditMode=false;
+
+  @ViewChild(MatPaginator,{static:true})
+  paginator!:MatPaginator;
+  @ViewChild(MatSort)
+  sort!:MatSort;
+  @ViewChild('notificationForm',{static:true})
+  notificationForm!:NgForm;
+
+
+  constructor(private notificationsService: notificationsService) {
+    this.notificationsData={} as Notification;
+    this.dataSource=new MatTableDataSource<any>();
+  }
 
   ngOnInit(): void {
+    this.dataSource.paginator=this.paginator;
+    this.getAllNotifications();
+  }
+
+  getAllNotifications(){
+    this.notificationsService.getAll().subscribe((response:any)=>{
+      this.dataSource.data=response;
+    });
   }
 
 }
