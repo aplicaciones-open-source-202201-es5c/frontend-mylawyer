e9e7003cd4bd3cb6e6f982ecec9e789913fbd0d3
Andres Reynoso <u202010196@upc.edu.pe>
2022-05-05 19:14:16 -0500

    Corrected Views
    It features a profile view after the user clicks another segment.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppointmentsComponent } from './appointments/pages/appointments/appointments.component';
+import { AppointmentsComponent} from "./appointments/pages/appointments/appointments.component";
 import { ClientsComponent} from "./clients/pages/clients/clients.component";
 import { PagesComponent} from "./notifications/pages/notifications/pages.component";
 import {IndexComponent} from "./index/pages/index.component";
@@ -9,6 +9,7 @@ import {IndexComponent} from "./index/pages/index.component";
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'profile', component: IndexComponent },
   { path: 'clients', component: ClientsComponent },
   { path: 'appointments', component: AppointmentsComponent },
   { path: 'notifications', component: PagesComponent }
